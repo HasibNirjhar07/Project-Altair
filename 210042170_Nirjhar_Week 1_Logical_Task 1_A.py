26de5a17344e3a5866bec43295a69134863039d3
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2023-07-27 23:26:26 +0600

    Update 210042170_Nirjhar_Week 1_Logical_Task 1_A.py
    Final code

--- 210042170_Nirjhar_Week 1_Logical_Task 1_A.py
+++ 210042170_Nirjhar_Week 1_Logical_Task 1_A.py
@@ -1,3 +1,22 @@
+from collections import deque
+
+def bfs(source, adj, v1):
+    q = deque()
+    v = v1
+    c=0
+    visited = [False] * v
+    q.append(source)
+    visited[source] = True
+
+    while q:
+        f = q.popleft()
+        c+=1
+
+        for nums in adj[f]:
+            if not visited[nums]:
+                q.append(nums)
+                visited[nums] = True
+
 v = int(input())
 adj = [[] for _ in range(v)]
 
@@ -7,9 +26,15 @@ for _ in range(n):
   adj[x].append(y)
   adj[y].append(x)
 
-for i in range(v):
-  print(f"{i}-->", end="")
-  for node in adj[i]:
-    print(node, end=",")
-  print()
+# for i in range(v):
+#   print(f"{i}-->", end="")
+#   for node in adj[i]:
+#     print(node, end=",")
+#   print()
 
+c=bfs(0,adj,v)
+
+if c == v:
+        print("True")
+else:
+        print("False")
